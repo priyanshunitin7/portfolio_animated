f94181c23ea7471e3332c53ddc120fb72b4e7361
priyanshunitin7 <nraj67609@gmail.com>
2026-03-22 23:46:10 +0530

    fix: migrate to new lenis API

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,12 +1,28 @@
 "use client";
 
-import { ReactLenis } from "@studio-freight/react-lenis";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function SmoothScroll({ children }: { children: any }) {
-  return (
-    <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true, easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)) }}>
-      {children}
-    </ReactLenis>
-  );
+export default function SmoothScroll({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.05,
+      duration: 1.5,
+      smoothWheel: true,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
 }
